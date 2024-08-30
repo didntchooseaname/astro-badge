009a6e5d3885894e8ad9e24f55185c18863bbb74
didntchooseaname <126179594+didntchooseaname@users.noreply.github.com>
2024-08-30 21:30:01 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.VERCEL_ENV === 'production' ? 'https://astro.badg.es/' : process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/` : 'http://localhost:4321/',
-  output: 'hybrid',
   adapter: vercel(),
   integrations: [
     tailwind({ applyBaseStyles: false }),
@@ -19,9 +17,5 @@ export default defineConfig({
     optimizeDeps: { exclude: ['@resvg/resvg-js'] },
     build: { rollupOptions: { external: ["@resvg/resvg-js"] } },
     // Expose Vercel’s analytics ID to client-side scripts.
-    define: {
-      'import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID':
-        JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
-    },
   }
 });
